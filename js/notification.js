14c394c24342449af51f2bb3ac6fbcd90804078a
rustiandi <99311985+rustiandi@users.noreply.github.com>
2025-09-14 05:54:02 +0700

    Create notification.js

---
+++ js/notification.js
@@ -0,0 +1,56 @@
+function notification(target, time) {
+	var a = "#" + target;
+	$(".notification-box").removeClass("show");
+	setTimeout(() => {
+		$(a).addClass("show");
+	}, 300);
+	if (time) {
+		time = time + 300;
+		setTimeout(() => {
+			$(".notification-box").removeClass("show");
+		}, time);
+	}
+};
+
+$(".notification-box .close-button").click(function (event) {
+	event.preventDefault();
+	$(".notification-box.show").removeClass("show");
+});
+
+$(".notification-box.tap-to-close .notification-dialog").click(function () {
+	$(".notification-box.show").removeClass("show");
+});
+
+function invitationNotif(couple, subtitle, text) {
+	var html
+	html = '<div class="notification-dialog ios-style">' +
+		'<div class="notification-header">' +
+		'<div class="in">' +
+		'<img src="https://pazvanka.github.io/logo/logo.svg" alt="image" class="imaged w24 rounded">' +
+		'<strong>' + couple + '</strong>' +
+		'</div>' +
+		'</div>' +
+		'<div class="notification-content">' +
+		'<div class="in" id="notificationText">' +
+		'<h3 class="subtitle">' + subtitle + '</h3>' +
+		'<div class="text">' + text + '</div>' +
+		'</div>' +
+		'</div>' +
+		'</div>';
+	$('#notification').html(html);
+	notification('notification', 5000);
+}
+
+//Show input error messages
+function showError(input, message) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-notif error mb-3';
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+}
+
+//show success colour
+function showSucces(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-notif mb-3';
+}
